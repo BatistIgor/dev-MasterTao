beef9e98b7265970f4402498b649bae19c2bb835
BatistIgot <igorbatist089@gmail.com>
2024-06-02 20:39:42 +0300

    feat: header done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,28 @@
-  
+function moveButton() {
+    const btn = document.querySelector('.header__btn');
+    const list = document.querySelector('.header__navigation');
+    const nav = document.querySelector('.header__inner');
+
+    if (window.innerWidth < 1024) {
+        list.appendChild(btn);
+    } else {
+        nav.appendChild(btn);
+    }
+}
+
+// Перемещение кнопки при загрузке страницы
+moveButton();
+
+// Перемещение кнопки при изменении размера окна
+window.addEventListener('resize', moveButton);
+
+
+const burger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.header__navigation');
+
+function hamburger() {
+    burger.classList.toggle("is-active");
+    navigation.classList.toggle("show");
+}
+
+burger.addEventListener('click', hamburger)
